b1598618248a9e36b4b6253db2d8a9e9fc92f75e
belal-allala <belalallala810@gmail.com>
2025-09-26 11:11:46 +0100

    feat(dao): Implémentation des opérations CRUD dans AbonnementDAO

--- src/main/dao/AbonnementDAO.java
+++ src/main/dao/AbonnementDAO.java
@@ -1,17 +1,45 @@
 package main.dao;
 
 import main.entity.Abonnement;
+import main.entity.AbonnementAvecEngagement;
+import main.entity.AbonnementSansEngagement;
+import main.entity.StatutAbonnement;
 import main.util.DatabaseManager;
+
+
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AbonnementDAO {
-    private final DatabaseManager dbManager;
 
-    public AbonnementDAO() {
-        this.dbManager = new DatabaseManager();
+    public void create(Abonnement abonnement) {
+
+        String sql="INSERT INTO abonnement(id, nomService, montantMensuel, dateDebut, dateFin, statut, typeAbonnement, dureeEngagementMois) VALUES(?,?,?,?,?,?,?,?)";
+
+        try(Connection conn = DatabaseManager.getConnection(); 
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                
+            pstmt.setString(1, abonnement.getId());
+            pstmt.setString(2, abonnement.getNomService());
+            pstmt.setDouble(3, abonnement.getMontantMensuel());
+            pstmt.setDate(4, Date.valueOf(abonnement.getDateDebut()));
+            pstmt.setDate(5, Date.valueOf(abonnement.getDateFin()));
+            pstmt.setString(6, abonnement.getStatut().name());
+
+            if (abonnement instanceof AbonnementAvecEngagement) {
+                pstmt.setString(7, "AVEC_ENGAGEMENT");
+                pstmt.setInt(8, ((AbonnementAvecEngagement) abonnement).getDureeEngagementMois());
+            } else {
+                pstmt.setString(7, "SANS_ENGAGEMENT");
+                pstmt.setNull(8, Types.INTEGER);
+            }
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
     }
 
-    // TODO: Implement CRUD operations for Abonnement
 }
