d0095b34a3a28d0c41db61f0d4c6b3d7abb864d5
belal-allala <belalallala810@gmail.com>
2025-09-26 12:17:35 +0100

    feat(util): Mise à jour de la configuration de base de données pour PostgreSQL

--- src/main/util/DatabaseManager.java
+++ src/main/util/DatabaseManager.java
@@ -5,16 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:mysql://localhost:3306/subscription_manager";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:postgresql://localhost:5432/abonnement";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "belal";
 
-    public Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
-            throw new SQLException("Driver MySQL non trouvé", e);
+            throw new SQLException("Driver MySQL non trouve", e);
         }
     }
 
