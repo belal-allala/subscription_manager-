b9a87ab5bde193bb1cb968119f5d2b2fe8314446
belal-allala <belalallala810@gmail.com>
2025-09-26 11:09:33 +0100

    feat(entity): Implémentation complète de la classe Paiement avec nouveaux attributs

--- src/main/entity/Paiement.java
+++ src/main/entity/Paiement.java
@@ -3,21 +3,70 @@ package main.entity;
 import java.time.LocalDate;
 
 public class Paiement {
-    private int id;
-    private int abonnementId;
-    private double montant;
+    private String idPaiement;
+   private String idAbonnement;
+    private LocalDate dateEcheance;
     private LocalDate datePaiement;
+    private String typePaiement;
     private StatutPaiement statut;
 
     public Paiement() {}
 
-    public Paiement(int abonnementId, double montant, LocalDate datePaiement) {
-        this.abonnementId = abonnementId;
-        this.montant = montant;
+    public Paiement(String idPaiement, String idAbonnement,LocalDate dateEcheance, LocalDate datePaiement, String typePaiement) {
+        this.idPaiement = idPaiement;
+        this.idAbonnement = idAbonnement;
+        this.dateEcheance = dateEcheance;
+        this.typePaiement = typePaiement;
         this.datePaiement = datePaiement;
         this.statut = StatutPaiement.EN_ATTENTE;
     }
 
     // Getters et Setters
-    // TODO: Implement getters and setters
+    public String getIdPaiement() {
+        return idPaiement;
+    }
+
+    public void setIdPaiement(String idPaiement) {
+        this.idPaiement = idPaiement;
+    }
+
+    public String getIdAbonnement() {
+        return idAbonnement;
+    }
+
+    public void setIdAbonnement(String idAbonnement) {
+        this.idAbonnement = idAbonnement;
+    }
+
+    public LocalDate getDateEcheance() {
+        return dateEcheance;
+    }
+
+    public void setDateEcheance(LocalDate dateEcheance) {
+        this.dateEcheance = dateEcheance;
+    }
+
+    public LocalDate getDatePaiement() {
+        return datePaiement;
+    }
+
+    public void setDatePaiement(LocalDate datePaiement) {
+        this.datePaiement = datePaiement;
+    }
+
+    public String getTypePaiement() {
+        return typePaiement;
+    }
+
+    public void setTypePaiement(String typePaiement) {
+        this.typePaiement = typePaiement;
+    }
+
+    public StatutPaiement getStatut() {
+        return statut;
+    }
+    
+    public void setStatut(StatutPaiement statut) {
+        this.statut = statut;
+    }
 }
