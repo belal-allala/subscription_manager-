4d04bd76ef49bdb7b5d9aad2a282cef0cc4117b9
belal-allala <belalallala810@gmail.com>
2025-09-26 10:47:56 +0100

    feat(entity): Implémentation complète de la classe Abonnement avec nouveaux attributs et getters/setters

--- src/main/entity/Abonnement.java
+++ src/main/entity/Abonnement.java
@@ -3,22 +3,70 @@ package main.entity;
 import java.time.LocalDate;
 
 public abstract class Abonnement {
-    protected int id;
+    protected String id;
+    protected String nomService;
     protected LocalDate dateDebut;
     protected LocalDate dateFin;
-    protected double montant;
+    protected double montantMensuel;
     protected StatutAbonnement statut;
 
-    // Constructeurs, getters et setters
     public Abonnement() {}
 
-    public Abonnement(LocalDate dateDebut, LocalDate dateFin, double montant) {
+    public Abonnement(String id, String nomService, LocalDate dateDebut, LocalDate dateFin, double montantMensuel) {
+        this.id = id;
+        this.nomService = nomService;
         this.dateDebut = dateDebut;
         this.dateFin = dateFin;
-        this.montant = montant;
+        this.montantMensuel = montantMensuel;
         this.statut = StatutAbonnement.ACTIF;
     }
 
-    // Getters et Setters
-    // TODO: Implement getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNomService() {
+        return nomService;
+    }
+
+    public void setNomService(String nomService) {
+        this.nomService = nomService;
+    }
+
+    public double getMontantMensuel() {
+        return montantMensuel;
+    }
+
+    public void setMontantMensuel(double montantMensuel) {
+        this.montantMensuel = montantMensuel;
+    }
+
+    public LocalDate getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDate dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDate getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDate dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public StatutAbonnement getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutAbonnement statut) {
+        this.statut = statut;
+    }
+   
 }
